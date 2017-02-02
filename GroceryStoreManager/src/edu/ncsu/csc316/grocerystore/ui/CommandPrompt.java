4ab6e18fd63fd563221f81eb12e7661f4a8b30a5
NBoar <NBoar@Leonard>
2017-02-01 21:30:01 -0500

    More javadoc

--- GroceryStoreManager/src/edu/ncsu/csc316/grocerystore/ui/CommandPrompt.java
+++ GroceryStoreManager/src/edu/ncsu/csc316/grocerystore/ui/CommandPrompt.java
@@ -2,15 +2,28 @@ package edu.ncsu.csc316.grocerystore.ui;
 
 import java.util.Scanner;
 
+/**
+ * Interacts with the user via command line in order
+ * to retrieve filepath input.
+ * @author Nicholas Board (ndboard)
+ */
 public class CommandPrompt {
 
+	/** The filepath for the customer file. */
 	String customerFile;
+	/** The filepath for the product file. */
 	String orderFile;
 	
+	/**
+	 * CommandPrompt constructor, does nothing.
+	 */
 	public CommandPrompt() {
 		// This constructor is intentionally empty. Nothing special is needed here.
 	}
 	
+	/**
+	 * Prints queries and takes input via command line.
+	 */
 	public void interact() {
 		Scanner commandScan = new Scanner(System.in);
 		
@@ -24,18 +37,34 @@ public class CommandPrompt {
 		commandScan.close();
 	}
 	
+	/**
+	 * Sets the filepath for the customer file.
+	 * @param fileName User-input filepath for the customer file.
+	 */
 	private void setCustomerFile(String fileName) {
 		customerFile = fileName;
 	}
 	
+	/**
+	 * Sets the filepath for the product file.
+	 * @param fileName User-input filepath for the product file.
+	 */
 	private void setOrderFile(String fileName) {
 		orderFile = fileName;
 	}
 	
+	/**
+	 * Getter for the customer filepath.
+	 * @return customerFile the path of the customer file.
+	 */
 	public String getCustomerFile() {
 		return customerFile;
 	}
 	
+	/**
+	 * Getter for the product filepath.
+	 * @return orderFile the path of the product file.
+	 */
 	public String getOrderFile() {
 		return orderFile;
 	}
--- GroceryStoreManager/src/edu/ncsu/csc316/grocerystore/ui/Driver.java
+++ GroceryStoreManager/src/edu/ncsu/csc316/grocerystore/ui/Driver.java
@@ -1,16 +1,25 @@
 package edu.ncsu.csc316.grocerystore.ui;
 
+/**
+ * Contains the main method that starts the program
+ * and the GroceryStoreManager object.
+ * @author Nicholas Board (ndboard)
+ */
 public class Driver {
 	
+	/** The filepath for the customer file. */
 	static String customerFile;
+	/** The filepath for the product file. */
 	static String orderFile;
 	
+	/**
+	 * Where the program starts.
+	 * @param args Would contain arguments.
+	 */
 	public static void main(String args[]) {
 		
 		CommandPrompt cp = new CommandPrompt();
 		cp.interact();
-	
 		
 	}
-
 }
