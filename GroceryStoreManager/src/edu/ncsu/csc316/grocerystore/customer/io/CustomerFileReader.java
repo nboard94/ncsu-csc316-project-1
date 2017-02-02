d02868d120ff0b29961644b44a11b44f5263f137
NBoar <NBoar@Leonard>
2017-02-01 22:09:08 -0500

    Update to CustomerFileReader and Testing

--- GroceryStoreManager/src/edu/ncsu/csc316/grocerystore/customer/io/CustomerFileReader.java
+++ GroceryStoreManager/src/edu/ncsu/csc316/grocerystore/customer/io/CustomerFileReader.java
@@ -1,5 +1,7 @@
 package edu.ncsu.csc316.grocerystore.customer.io;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import edu.ncsu.csc316.grocerystore.customer.Customer;
@@ -15,21 +17,32 @@ public class CustomerFileReader {
 	private ArrayBasedList<Customer> data = new ArrayBasedList<Customer>();
 	/** The current customer while looping. */
 	private Customer currentCust = new Customer("", "", "", 0);
+	
+	private String currentLine;
+	
+	private String[] currentData;
 
 	/**
 	 * Scans in a file, adding customer data to the ArrayBasedList.
 	 * @param filePath The path to the customer file.
+	 * @throws FileNotFoundException 
 	 */
-	public CustomerFileReader(String filePath) {
+	public CustomerFileReader(String filePath) throws FileNotFoundException {
 		
-		Scanner scan = new Scanner(filePath);
-		scan.useDelimiter(",");
+		File file = new File(filePath);
+		Scanner scan = new Scanner(file);
+		currentLine = scan.nextLine();
+		currentData = currentLine.split(",");
+		
+		for(int i = 0; i < currentData.length; i++) {
+			currentData[i] = currentData[i].trim();
+		}
 		
 		while (scan.hasNextLine()) {
-			currentCust.setCustomerID(scan.next());
-			currentCust.setCustomerName(scan.next());
-			currentCust.setCustomerState(scan.next());
-			currentCust.setCustomerZipcode(Integer.valueOf(scan.next()));
+			currentCust.setCustomerID(currentData[0]);
+			currentCust.setCustomerName(currentData[1]);
+			currentCust.setCustomerState(currentData[2]);
+			currentCust.setCustomerZipcode(Integer.valueOf(currentData[3]));
 			
 			data.add(currentCust);
 		}
--- GroceryStoreManager/test/edu/ncsu/csc316/grocerystore/io/CustomerFileReaderTest.java
+++ GroceryStoreManager/test/edu/ncsu/csc316/grocerystore/io/CustomerFileReaderTest.java
@@ -1,5 +1,15 @@
 package edu.ncsu.csc316.grocerystore.io;
 
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
+
+import edu.ncsu.csc316.grocerystore.customer.Customer;
+import edu.ncsu.csc316.grocerystore.customer.io.CustomerFileReader;
+import edu.ncsu.csc316.grocerystore.list.ArrayBasedList;
+
 /**
  * Testing class for the CustomerFileReader object.
  * @author Nicholas Board (ndboard)
@@ -7,7 +17,17 @@ package edu.ncsu.csc316.grocerystore.io;
 public class CustomerFileReaderTest {
 
 	/** Filepath of the small customers testing file. */
-	String testFile = "/GroceryStoreManager/input/customers_small.txt";
+	String testFile = "input/customers_small.txt";
+	
+	@Test
+	public void testCustomerFileReader() throws FileNotFoundException {
+		
+		CustomerFileReader custReadTest = new CustomerFileReader(testFile);
+		
+		ArrayBasedList<Customer> data = custReadTest.getData();
+		
+		assertEquals(50000, data.size());
+	}
 	
 	
 
